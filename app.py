6bd87908554a7ed86d4a8091ef8903ae9e5851f5
Auricdhero <datauric@gmail.com>
2019-10-24 16:03:43 +0000

    changing my db

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
+import os
+import json
+
 from flask import Flask, flash, url_for, redirect, render_template, request, session
 from passlib.hash import sha256_crypt
-from flask_mysqldb import MySQL
+from flask_pymongo import PyMongo
+from flask import request
 import yaml
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/preptime_db"
+mongo = PyMongo(app)
 
 # db
 
 
-mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-    
+        
         return render_template('students.html', name=NameError)
 
     return render_template('index.html', name=NameError)
