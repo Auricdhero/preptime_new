3546ebe43cbe2929766c30c62d47e23c0bfb8e89
Auricdhero <datauric@gmail.com>
2019-12-03 23:43:58 +0000

    Figured out the database

--- app.py
+++ app.py
@@ -1,32 +1,69 @@
 from flask import Flask, flash, url_for, redirect, render_template, request, session
+from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
+from flask_user import login_required, SQLAlchemyAdapter, UserManager, UserMixin
+#from flask import roles_required
 from passlib.hash import sha256_crypt
-from flask_mysqldb import MySQL
+import mysql.connector
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = ''
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'preptime_admin, preptime_students, preptime_school'
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['MYSQL_HOST'] = '35.187.117.190'
+app.config['MYSQL_USER'] = 'auric'
+app.config['MYSQL_PASSWORD'] = 'auric'
+app.config['MYSQL_DB'] = 'preptime_db'
 
-mysql = MySQL(app)
+#mysql = MySQL(app)
+mydb = mysql.connector.connect(
+  host="35.187.117.190",
+  user="auric",
+  passwd="auric",
+  database="preptime_db"
+)
 
+print(mydb)
 
-#run home termplate
+#run home template
 @app.route('/', methods=['GET', 'POST'])
+#@roles_required('sch_Admin')
 def index():
     if request.method == "POST":
         details = request.form
+        print('Hello POST')
+        name = details['name']
         email = details['email']
-        password = details['pswd']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO preptime_school(email, password) VALUES (%c, %c)", (email, password))
-        mysql.connection.commit()
-        cur.close()
-        return render_template("schdashboard.html", name=NameError)
+        password = details['psw']
+        repeat_password = details['psw-repeat']
+        #cur = mysql.connection.cursor()
+        #cur.execute("INSERT INTO preptime_users(email, password) VALUES (%c, %c)", (email, password, repeat_pasword))
+        
+        #cur.execute("INSERT INTO preptime_users(email, password) VALUES ('hello', 'home')")
+        #mysql.connection.commit()
+        #cur.close()
+        mycursor = mydb.cursor()
+
+        sql = "INSERT INTO preptime_users(name, email, password) VALUES (%c, %c, %c)"
+        val = (name, email, password)
+        mycursor.execute("INSERT INTO preptime_users(email, password) VALUES ('hello', 'home')")
+
+        mydb.commit()
+        return 'done'
+    print('Hello GET')
     return render_template('index.html')
 
-    
+#prepadmin portal
+#@roles_required('Admin')
+
+#schooladmin portal
+#@roles_required('sch_Admin')
+
+
+#for student portal
+#@roles_required('student')
+
+
+
 
 #payment run templates
 def visa():
@@ -39,27 +76,6 @@ def vfcash():
     return render_template('vfcash.htm', name=NameError)
 def atmoney():
     return render_template('atm.htm', name=NameError)
-
-
-#prepadmin portal
-def prepdashboard():
-    if request.method == "POST":
-        details = request.form
-        
-    return render_template("index.html", user=user)
-
-#schooladmin portal
-def schdashboard():
-    
-    return render_template("schdashboard.html", user=user)
-
-
-#for student portal
-def students():
-    
-    return render_template("student.html", user=user)
-
-
 
 
 
